867fe5b5348367211134114846f1f941adbc64f3
hotcat190 <nguyendinhnguyen151204@gmail.com>
2025-04-07 15:28:22 +0700

    up

---
+++ tcp_server.py
@@ -0,0 +1,16 @@
+import socket
+
+serverPort = 12000
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+print('The server is ready to receive')
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024)
+    print(sentence.decode())  
+    capitalizedSentence = sentence.decode().upper() 
+    connectionSocket.send(capitalizedSentence.encode()) 
+    connectionSocket.close()
